5e9e10d47abd66a2526a33de25dc84417644688e
anavaleska2908 <avaleska2908@hotmail.com>
2022-12-18 11:55:30 -0300

    feature: Creation of use-case bringing all notifications from a user, as well as their respective test.

---
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,27 @@
+import { makeNotification } from "@test/factories/notification-factory";
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
+import { CountRecipientNotifications } from "./count-recipient-notifications";
+import { GetRecipientNotifications } from "./get-recipient-notifications";
+
+describe('Get recipients notifications', () => {
+  it('Should be able to get recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const getRecipientsNotifications = new GetRecipientNotifications(notificationsRepository);
+
+    await notificationsRepository.create(makeNotification({recipientId: 'recipient-1'}));
+
+    await notificationsRepository.create(makeNotification({recipientId: 'recipient-1'}));
+
+    await notificationsRepository.create(makeNotification({recipientId: 'recipient-2'}));
+
+    const { notifications } = await getRecipientsNotifications.execute({
+      recipientId: 'recipient-1',
+    })
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(expect.arrayContaining([
+      expect.objectContaining({ recipientId: 'recipient-1' }),
+      expect.objectContaining({ recipientId: 'recipient-1' }),
+    ]))
+  })
+})
